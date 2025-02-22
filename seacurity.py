bf8088001489566d261a556ab009d7b8cad975cc
Mac <redsbaniasadi1381@gmail.com>
2025-02-22 08:25:00 +0000

    hash password

--- seacurity.py
+++ seacurity.py
@@ -4,3 +4,6 @@ from passlib.context import CryptContext
 from config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
