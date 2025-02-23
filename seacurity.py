5c0c4aaf33d7e127cb835448a917e61f15d97fbb
Mac <redsbaniasadi1381@gmail.com>
2025-02-23 10:05:00 +0000

    add access

--- seacurity.py
+++ seacurity.py
@@ -11,3 +11,10 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
+
+
+def create_access_token(data: dict, expires_delta: int = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_delta or settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
