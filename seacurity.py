7cd043e594e569fdc20875c7d91a1715097ee1f4
Mac <redsbaniasadi1381@gmail.com>
2025-02-23 10:05:00 +0000

    decode token jwt

--- seacurity.py
+++ seacurity.py
@@ -18,3 +18,10 @@ def create_access_token(data: dict, expires_delta: int = None):
     expire = datetime.utcnow() + timedelta(minutes=expires_delta or settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+def decode_access_token(token: str):
+    try:
+        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    except JWTError:
+        return None
