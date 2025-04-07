b74db89cfa790f2198551ba6c3414a2e2f27cdb5
Mac <redsbaniasadi1381@gmail.com>
2025-04-07 09:08:05 +0000

    get current user for test secure

--- api.py
+++ api.py
@@ -16,3 +16,14 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
     hashed_pw = hash_password(user.password)
     return crud.create_user(db, schemas.UserCreate(email=user.email, password=hashed_pw))
+
+
+def get_current_user(token: str = Depends(models.oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+    email = payload.get("sub")
+    user = crud.get_user_by_email(db, email=email)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user``
