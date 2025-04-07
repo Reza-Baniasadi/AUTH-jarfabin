5a599b4a81f20b42c5fc14f35ca0a02ee5232905
Mac <redsbaniasadi1381@gmail.com>
2025-04-07 09:08:05 +0000

    last change

--- api.py
+++ api.py
@@ -6,9 +6,6 @@ import crud, schemas, models
 from database import get_db
 
 
-
-
-
 @app.post("/auth/register")
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
