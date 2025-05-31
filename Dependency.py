d1a27cee13a1b62b42104f57b6ab22825658bcc0
Mac <redsbaniasadi1381@gmail.com>
2025-05-31 10:44:03 +0000

    session

--- Dependency.py
+++ Dependency.py
@@ -5,3 +5,12 @@ import crud, models
 from database import get_db
 from auth import decode_access_token
 import logging
+
+logger = logging.getLogger("pid_secure")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+_active_pids = {}
